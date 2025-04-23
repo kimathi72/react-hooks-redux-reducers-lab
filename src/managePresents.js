23efb2070ec4d3eb96f0a6eb53126ed9a802870d
roy kimathi <kimathiwaweru@gmail.com>
2025-04-23 11:15:46 +0300

    lab complete

--- src/managePresents.js
+++ src/managePresents.js
@@ -2,4 +2,12 @@ const initialState = { numberOfPresents: 0 };
 
 export function managePresents(state = initialState, action) {
   // your code here
+  switch (action.type) {
+    case "presents/increase":
+      state = { numberOfPresents: state.numberOfPresents + 1}
+      return state
+  
+    default:
+      return state;
+  }
 }
